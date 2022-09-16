2f4f9a30307230cbfc24533fc2c9d727a1103eea
Hoang Son <hoangvansonit@gmail.com>
2022-09-16 22:06:26 +0700

    create middleware with express

--- app.js
+++ app.js
@@ -1,38 +1,13 @@
-const http = require('http');
+const express = require('express'); // import express
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === '/') {
-    res.setHeader('Content-Title', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Lab2.1</title></head>');
-    res.write(
-      '<body><form action="/create-user" method="POST"><input type="text" name="user"><button type="submit">Add User</button></form></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/users') {
-    res.setHeader('Content-Title', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Lab2.1</title></head>');
-    res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/create-user') {
-    const body = [];
-    req.on('data', chunk => {
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody.split('=')[1]);
-    });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    res.end();
-  }
+const app = express();
+
+app.use('/users', (req, res, next) => {
+  res.send('<p>The Middleware that handles just /users</p>');
 });
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  res.send('<p>The Middleware that handles just /</p>');
+});
+
+app.listen(3000);
